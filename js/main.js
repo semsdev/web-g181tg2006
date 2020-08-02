f6eebcbed24cca6d7d1c468827b1f4c89363e957
semsev <semsettn@gmail.com>
2020-08-03 06:55:46 +0900

    showcase update

--- js/main.js
+++ js/main.js
@@ -6,3 +6,35 @@ $('.Header-bars').on('click', function () {
     $('.Header').addClass('menu-show');
   }
 });
+
+var $carousel = $('.Highlights-slider').flickity({
+  cellAlign: 'left',
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+});
+var flkty = $carousel.data('flickity');
+
+$('.Highlights-item:eq(0)').addClass('is-expanded');
+$carousel.flickity('reposition');
+
+$carousel.on('staticClick.flickity', function (
+  event,
+  pointer,
+  cellElement,
+  cellIndex
+) {
+  if (!cellElement) {
+    return;
+  }
+  $carousel.find('.is-expanded').removeClass('is-expanded');
+  $(cellElement).addClass('is-expanded');
+  $carousel.flickity('reposition');
+  $carousel.flickity('select', cellIndex);
+});
+
+$carousel.on('select.flickity', function () {
+  $carousel.find('.is-expanded').removeClass('is-expanded');
+  $('.Highlights-item:eq(' + flkty.selectedIndex + ')').addClass('is-expanded');
+  $carousel.flickity('reposition');
+});
